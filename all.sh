9a0c46149ad39ef25b1f757c3dd227fa4b93ac2e
Jan Bölsche <regular.gonzales@googlemail.com>
2018-06-05 18:04:05 +0200

    Add suport for local base images

--- all.sh
+++ all.sh
@@ -1,10 +1,18 @@
 #!/bin/sh -exu
-./install-arch-linux-rpi-zero-w.sh $1
-echo "*** Setting up logins ***"
+
+echo "\n*** Installing base system ***"
+if [ $# -gt 1 ] ; then
+  ./install-arch-linux-rpi-zero-w.sh $1 $2
+else
+  ./install-arch-linux-rpi-zero-w.sh $1
+fi
+
+echo "\n*** Setting up logins ***"
 ./setup-logins.sh $1
-echo "*** Hardening ssh ***"
+echo "\n*** Hardening ssh ***"
 ./setup-ssh.sh $1
-echo "*** Setting up WIFI ***"
+echo "\n*** Setting up WIFI ***"
 ./setup-wifi.sh $1
+echo "\n*** Setting hostname ***"
 ./setup-hostname.sh $1
-echo "*** Done ***"
+echo "\n*** Done ***"
--- install-arch-linux-rpi-zero-w.sh
+++ install-arch-linux-rpi-zero-w.sh
@@ -5,33 +5,43 @@ if [ "$(id -u)" == "0" ]; then
 fi
 
 dev=$1
-cd $(mktemp -d)
+tmp=$(mktemp -d)
 
-function umountboot {
-    sudo umount boot || true
-    sudo umount root || true
+function cleanup {
+    sudo umount $tmp/boot || true
+    sudo umount $tmp/root || true
 }
 
-# RPi1/Zero (armv6h):
-archlinux=/tmp/ArchLinuxARM-rpi-latest.tar.gz
-url=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
+if [ $# -lt 2 ] ; then
+  # RPi1/Zero (armv6h):
+  archlinux=/tmp/ArchLinuxARM-rpi-latest.tar.gz
+  url=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
 
-# RPi2 (armv7h):
-# archlinux=/tmp/ArchLinuxARM-rpi-2-latest.tar.gz
-# url=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
+  # RPi2 (armv7h):
+  # archlinux=/tmp/ArchLinuxARM-rpi-2-latest.tar.gz
+  # url=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
 
-curl -L -o $archlinux -z $archlinux $url
+  curl -L -o $archlinux -z $archlinux $url
+else
+  archlinux=$2
+fi
+
+if ! [ -f $archlinux ] ; then
+  echo "File not found: $archlinux"
+  exit 1
+fi
+
 sudo parted -s $dev mklabel msdos
 sudo parted -s $dev mkpart primary fat32 1 128
 sudo parted -s $dev mkpart primary ext4 128 -- -1
 sudo mkfs.vfat ${dev}1
 sudo mkfs.ext4 -F ${dev}2
-mkdir -p boot
-sudo mount ${dev}1 boot
-trap umountboot EXIT
-mkdir -p root
-sudo mount ${dev}2 root
+mkdir -p $tmp/boot
+sudo mount ${dev}1 $tmp/boot
+trap cleanup EXIT
+mkdir -p $tmp/root
+sudo mount ${dev}2 $tmp/root
 
-sudo bsdtar -xpf $archlinux -C root
+sudo bsdtar -xpf $archlinux -C $tmp/root
+sudo mv $tmp/root/boot/* $tmp/boot
 sudo sync
-sudo mv root/boot/* boot
